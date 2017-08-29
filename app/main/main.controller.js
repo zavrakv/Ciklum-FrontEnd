89dd635cbac988a3b214df822b0ec77e1fbe747d
Valentine <zavrak.valentin@gmail.com>
2017-08-29 23:46:19 +0300

    Lists created and autorefresh added

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -5,8 +5,10 @@
 
 	function mainController(FarmsFactory) {
 		var vm = this;
-  
-		
+    
+    vm.toggleFarmAutorefresh = toggleFarmAutorefresh;
+    vm.toggleServersGlobal = toggleServersGlobal;
+    vm.toggleAutorefresh = toggleAutorefresh;
     
     FarmsFactory.getAllFarms()
       .then(function (res) {
@@ -14,6 +16,40 @@
         vm.farms = res;
         vm.farms.selected = null
       });
+    
+    function toggleFarmAutorefresh(state, index) {
+      // console.log(!state, index)
+      // console.log(index)
+      toggleServersGlobal(!state, index)
+    }
+    
+    function toggleServersGlobal(state, index) {
+      angular.forEach(vm.farms[index].servers, function (server) {
+        server.toggle = state;
+      })
+    }
+    
+    function countToggled(index) {
+      var toggledCount = 0;
+      angular.forEach(vm.farms[index].servers, function (server) {
+        if(server.toggle) {
+          toggledCount += 1
+        }
+      });
+  
+      return toggledCount;
+    }
+    
+    function toggleAutorefresh(index) {
+      var count = countToggled(index);
+      var serversCount = vm.farms[index].servers.length;
+      console.log(count)
+      if (count === serversCount) {
+        vm.farms[index].toggle = true;
+      } else {
+        vm.farms[index].toggle = false;
+      }
+    }
     
   }
   
